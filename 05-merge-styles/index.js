ba223d7adcfa74288dfd60d49cb04e9ff1c81262
dzlek <6894255@gmail.com>
2024-10-02 11:48:51 +0300

    feat: do task5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const path = require('path');
+
+const targetFolder = 'styles';
+const resArray = [];
+
+fs.readdir(
+  path.join(__dirname, targetFolder),
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const cssFiles = files.filter(
+        (file) => file.isFile() && path.extname(file.name) === '.css',
+      );
+
+      files.forEach((file, index) => {
+        const fileExt = path.extname(file.name);
+        if (file.isFile() && fileExt === '.css') {
+          const filePath = path.join(__dirname, targetFolder, file.name);
+
+          fs.readFile(filePath, 'utf-8', (err, data) => {
+            if (err) throw err;
+            resArray.push(data);
+            console.log(data);
+            if (index === cssFiles.length - 1) {
+              fs.writeFile(
+                path.join(__dirname, 'project-dist', 'bundle.css'),
+
+                resArray.join('\n'),
+                (err) => {
+                  if (err) throw err;
+                  console.log('Файл был создан');
+                },
+              );
+            }
+          });
+        }
+      });
+    }
+  },
+);
+
+// fs.readFile(
+//   path.join(__dirname, 'styles', 'style.txt'),
+//   'utf-8',
+//   (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+//   },
+// );
+
+//   fs.stat(filePath, (err, stats) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(
+//         `${file.name.split('.')[0]} - ${fileExt.slice(
+//           1,
+//         )} - ${Math.round(stats.size / 1024)}kb`,
+//       );
+//     }
+//   }
+// );
