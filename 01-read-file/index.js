39dabbf9f86e158248d781ad808901f522494bb4
dzlek <6894255@gmail.com>
2025-01-13 17:56:31 +0300

    fix: add readstream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(path.join(__dirname, 'text.txt'), 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
+const filePath = path.join(__dirname, 'text.txt');
+
+const readStream = fs.createReadStream(filePath, 'utf8');
+
+readStream.on('data', (chunk) => {
+  process.stdout.write(chunk);
+});
+
+readStream.on('error', (err) => {
+  console.error('Error reading file:', err.message);
 });
